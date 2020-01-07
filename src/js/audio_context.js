5fb73dfda466a1147658487cd061dcb025a86c09
App Academy Student <email@email.com>
2020-01-07 11:57:09 -0500

    adds rotation

--- src/js/audio_context.js
+++ src/js/audio_context.js
@@ -18,14 +18,38 @@ const gainNodeTwo = audioContextTwo.createGain();
 const gainNodeThree = audioContextThree.createGain();
 
 const analyzerOne = audioContext.createAnalyser();
+analyzerOne.connect(audioContext.destination);
 
-trackOne.connect(analyzerOne).connect(gainNode).connect(audioContext.destination);
+trackOne.connect(analyzerOne);
+trackOne.connect(gainNode).connect(audioContext.destination);
 trackTwo.connect(gainNodeTwo).connect(audioContextTwo.destination);
 trackThree.connect(gainNodeThree).connect(audioContextThree.destination);
 
+////////////////////////////////////////////////////////////////
+
+console.log(analyzerOne);
 analyzerOne.fftSize = 512;
 let bufferLength = analyzerOne.frequencyBinCount;
 let dataArr = new Uint8Array(bufferLength);
+
+analyzerOne.getByteFrequencyData(dataArr);
+//slice arr into 2 halves
+
+let lowerHalfArr = dataArr.slice(0, (dataArr.length / 2) - 1);
+let upperHalfArr = dataArr.slice((dataArr.length / 2) - 1, dataArr.length - 1);
+
+let lowerMax = Math.max(...lowerHalfArr);
+let lowerAvg = avg(lowerHalfArr);
+let upperAvg = avg(upperHalfArr);
+
+let lowerMaxFr = lowerMax / lowerHalfArr.length;
+let lowerAvgFr = lowerAvg / lowerHalfArr.length;
+let upperAvgFr = upperAvg / upperHalfArr.length;
+
+
+
+///////////////////////////////////////////////////////////////
+
 
 const playButton = document.getElementById('btn-1');
 const playButton2 = document.getElementById('btn-2');
@@ -34,9 +58,6 @@ const playButton3 = document.getElementById('btn-3');
 const volumeControl = document.getElementById('vol-1');
 const volumeControlTwo = document.getElementById('vol-2');
 const volumeControlThree = document.getElementById('vol-3');
-
-
-
 
 playButton.addEventListener('click', function() {
     if (audioContext.state === 'suspended') {
@@ -104,29 +125,96 @@ volumeControlThree.addEventListener('input', function () {
     gainNodeThree.gain.value = this.value;
 });
 
+function avg(arr) {
+    let sum = 0;
+
+    arr.forEach(num => {
+        sum += num;
+    });
+
+    return (sum / arr.length);
+}
 
 function render() {
-    analyzerOne.getByteFrequencyData(dataArr);
-
-    //slice arr into 2 halves
-
-    let lowerHalfArr = dataArr.slice(0, (dataArr.length/2) - 1);
-    let upperHalfArr = dataArr.slice((dataArr.length/2) - 1, dataArr.length-1);
-
-    let lowerMax = max(lowerHalfArr);
-    let lowerAvg = avg(lowerHalfArr);
-    let upperAvg = avg(upperHalfArr);
-
-    let lowerMaxFr = lowerMax / lowerHalfArr.length;
-    let lowerAvgFr = lowerAvg / lowerHalfArr.length;
-    let upperAvgFr = upperAvg / upperHalfArr.length;
-
-    makeRoughGround(plane, modulate(upperAvgFr, 0, 1, 0.5, 4));
-    makeRoughGround(plane2, modulate(lowerMaxFr, 0, 1, 0.5, 4));
-
-    makeRoughBall(
-      ball,
-      modulate(Math.pow(lowerMaxFr, 0.5), 0, 1, 0, 8),
-      modulate(upperAvgFr, 0, 1, 0, 4)
-    );
+    group.rotation.y += 0.005;
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
 }
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+// const listener = new THREE.AudioListener();
+const group = new THREE.Group();
+camera.position.set(0, 0, 100);
+camera.lookAt(scene.position);
+// camera.add( listener );
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const planeGeometry = new THREE.PlaneGeometry(800, 800, 20, 20);
+const planeMaterial = new THREE.MeshLambertMaterial({
+    color: 0x6904ce,
+    side: THREE.DoubleSide,
+    wireframe: true
+});
+
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = -0.5 * Math.PI;
+plane.position.set(0, 30, 0);
+
+const plane2 = new THREE.Mesh(planeGeometry, planeMaterial);
+plane2.rotation.x = -0.5 * Math.PI;
+plane2.position.set(0, -30, 0);
+
+var geometry = new THREE.SphereGeometry(1, 12, 12);
+var material = new THREE.MeshBasicMaterial({
+    color: 0x6904ce,
+    wireframe: true
+});
+var sphere = new THREE.Mesh(geometry, material);
+
+sphere.position.set(0, 0, 0);
+
+group.add(plane);
+group.add(plane2);
+group.add(sphere);
+scene.add(group);
+camera.position.z = 5;
+console.log(dataArr);
+const animate = function () {
+    requestAnimationFrame(animate);
+
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
+
+    // analyzerOne.getByteFrequencyData(dataArr);
+    // //slice arr into 2 halves
+
+    // let lowerHalfArr = dataArr.slice(0, (dataArr.length / 2) - 1);
+    // let upperHalfArr = dataArr.slice((dataArr.length / 2) - 1, dataArr.length - 1);
+
+    // let lowerMax = Math.max(...lowerHalfArr);
+    // let lowerAvg = avg(lowerHalfArr);
+    // let upperAvg = avg(upperHalfArr);
+
+    // let lowerMaxFr = lowerMax / lowerHalfArr.length;
+    // let lowerAvgFr = lowerAvg / lowerHalfArr.length;
+    // let upperAvgFr = upperAvg / upperHalfArr.length;
+
+
+    renderer.render(scene, camera);
+};
+
+animate();
+
+
+
+render();
--- src/js/audio_visual_display.js
+++ src/js/audio_visual_display.js
@@ -1,10 +1,21 @@
-const THREE = require('three');
+// import "./audio_context.js";
+// const THREE = require('three');
 
 // const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// const camera = new THREE.PerspectiveCamera(
+//     75, 
+//     window.innerWidth / window.innerHeight,
+//     0.1,
+//     1000
+// );
 // const group = new THREE.Group();
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight);
+// camera.position.set(0, 0, 100);
+// camera.lookAt(scene.position);
+// scene.add(camera);
+
+// const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
 
 // const planeGeometry = new THREE.PlaneGeometry(800, 800, 20, 20);
 // const planeMaterial = new THREE.MeshLambertMaterial({
@@ -16,147 +27,31 @@ const THREE = require('three');
 // const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 // plane.rotation.x = -0.5 * Math.PI;
 // plane.position.set(0, 30, 0);
-// scene.add(plane);
-// scene.add(camera);
 
-// function animate() {
-//     requestAnimationFrame( animate );
-//     renderer.render( scene, camera )
-// }
-// animate();
-
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-    75, 
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
-const group = new THREE.Group();
-camera.position.set(0, 0, 100);
-camera.lookAt(scene.position);
-scene.add(camera);
-
-const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const planeGeometry = new THREE.PlaneGeometry(800, 800, 20, 20);
-const planeMaterial = new THREE.MeshLambertMaterial({
-    color: 0x6904ce,
-    side: THREE.DoubleSide,
-    wireframe: true
-});
-
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.rotation.x = -0.5 * Math.PI;
-plane.position.set(0, 30, 0);
-
-const plane2 = new THREE.Mesh(planeGeometry, planeMaterial);
-plane2.rotation.x = -0.5 * Math.PI;
-plane2.position.set(0, -30, 0);
-
-
-// const sphereGeometry = new THREE.IcosahedronGeometry(1, 1, 1);
-// const sphereMaterial = new THREE.MeshBasicMaterial({
-//     color: 0x6904ce,
-//     wireframe: true
-//   });
-// const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-
-var geometry = new THREE.SphereGeometry(1, 12, 12);
-var material = new THREE.MeshBasicMaterial({
-  color: 0x6904ce,
-  wireframe: true
-});
-var sphere = new THREE.Mesh(geometry, material);
-
-sphere.position.set(0, 0, 0);
-// sphere.scale.set(2, 1, 1);
-
-group.add(plane);
-group.add(plane2);
-group.add(sphere);
-scene.add(group);
-camera.position.z = 5;
-
-// renderer.render(scene, camera);
-
-const animate = function() {
-    requestAnimationFrame(animate);
-
-    sphere.rotation.x += 0.01;
-    sphere.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
-};
-
-animate();
-
-
-// var scene = new THREE.Scene();
-// var group = new THREE.Group();
-// var camera = new THREE.PerspectiveCamera(
-//     45,
-//     window.innerWidth / window.innerHeight,
-//     0.1,
-//     1000
-// );
-// camera.position.set(0, 0, 100);
-// camera.lookAt(scene.position);
-// scene.add(camera);
-
-// var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-// renderer.setSize(window.innerWidth, window.innerHeight);
-
-// var planeGeometry = new THREE.PlaneGeometry(800, 800, 20, 20);
-// var planeMaterial = new THREE.MeshLambertMaterial({
-//     color: 0x6904ce,
-//     side: THREE.DoubleSide,
-//     wireframe: true
-// });
-
-// var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-// plane.rotation.x = -0.5 * Math.PI;
-// plane.position.set(0, 30, 0);
-// group.add(plane);
-
-// var plane2 = new THREE.Mesh(planeGeometry, planeMaterial);
+// const plane2 = new THREE.Mesh(planeGeometry, planeMaterial);
 // plane2.rotation.x = -0.5 * Math.PI;
 // plane2.position.set(0, -30, 0);
-// group.add(plane2);
 
-// var icosahedronGeometry = new THREE.IcosahedronGeometry(10, 4);
-// var lambertMaterial = new THREE.MeshLambertMaterial({
-//     color: 0xff00ee,
-//     wireframe: true
+// var geometry = new THREE.SphereGeometry(1, 12, 12);
+// var material = new THREE.MeshBasicMaterial({
+//   color: 0x6904ce,
+//   wireframe: true
 // });
+// var sphere = new THREE.Mesh(geometry, material);
 
-// var ball = new THREE.Mesh(icosahedronGeometry, lambertMaterial);
-// ball.position.set(0, 0, 0);
-// group.add(ball);
+// sphere.position.set(0, 0, 0);
 
-// var ambientLight = new THREE.AmbientLight(0xaaaaaa);
-// scene.add(ambientLight);
-
-// var spotLight = new THREE.SpotLight(0xffffff);
-// spotLight.intensity = 0.9;
-// spotLight.position.set(-10, 40, 20);
-// spotLight.lookAt(ball);
-// spotLight.castShadow = true;
-// scene.add(spotLight);
-
-// // var orbitControls = new THREE.OrbitControls(camera);
-// // orbitControls.autoRotate = true;
-
+// group.add(plane);
+// group.add(plane2);
+// group.add(sphere);
 // scene.add(group);
+// camera.position.z = 5;
 
 // const animate = function() {
 //     requestAnimationFrame(animate);
 
-//     ball.rotation.x += 0.01;
-//     ball.rotation.y += 0.01;
+//     sphere.rotation.x += 0.01;
+//     sphere.rotation.y += 0.01;
 
 //     renderer.render(scene, camera);
 // };
